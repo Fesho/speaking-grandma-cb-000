2ef283bab6be97712b70a7ac4f70f3458b02984d
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-05 14:10:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,10 @@ if speak_to_grandma == "I LOVE YOU GRANDMA!"
 return "I LOVE YOU TOO PUMPKIN!!"
 
 elsif speak_to_grandma == speak_to_grandma.upcase 
-return"HUH?! SPEAK UP, SONNY!"
+return"HUH?! SPEAK UP, SONNY NO, NOT SINCE 1938!"
 
 else 
-return "NO, NOT SINCE 1938!"
+return "!"
 
 end
 end
